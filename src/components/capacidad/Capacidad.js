13b44e54789404a7ce419e137fd6c4b44734b8f4
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-01-30 10:49:12 -0300

    se obtiene tabla capacidad con filtro de rubro y luego de condicion

--- src/components/capacidad/Capacidad.js
+++ src/components/capacidad/Capacidad.js
@@ -93,8 +93,6 @@ const Capacidad = () => {
     // };
 
     const generaData = (infoCap) => {
-        console.log('Entra en generarData')
-
         const result = {};
 
         // Iterar sobre cada objeto del array
@@ -112,49 +110,13 @@ const Capacidad = () => {
             }
         });
 
-        console.log(result);
+        setTablaCap(result);
+
+        console.log("Tabla Capacidad Info: ", tablaCap)
 
         return result;
 };
 
-    // const generaData = (infoCap) => {
-    //     console.log('Entra en generarData')
-
-    //     //!FILTRO POR CONDICION: PROPIO / ALQUILADO
-
-    //     const CondP = infoCap.filter(info => info.condicion === "P");
-    //     const CondA = infoCap.filter(info => info.condicion === "A");
-
-    //     console.log("condP: ", CondP);
-    //     console.log("condA: ", CondA);
-
-    //     // //! FRILTRO SEGUN RUBROS
-
-    //     // const condPAgricultura = condP.filter(info => info.arubro_desc === "AGRICULTURA");
-    //     // const condAAgricultura = condA.filter(info => info.arubro_desc === "AGRICULTURA");
-        
-    //     // const condPGanaderia = condP.filter(info => info.arubro_desc === "GANADERIA");
-    //     // const condAGanaderia = condA.filter(info => info.arubro_desc === "GANADERIA");
-
-    //     // const condPTambo = condP.filter(info => info.arubro_desc === "TAMBO");
-    //     // const condATambo = condA.filter(info => info.arubro_desc === "TAMBO");
-
-    //     // const condPMixto = condP.filter(info => info.arubro_desc === "MIXTO");
-    //     // const condAMixto = condA.filter(info => info.arubro_desc === "MIXTO");
-
-    //     // console.log("AGRICULTURAP: ", condPAgricultura);
-    //     // console.log("AGRICULTURAA: ", condAAgricultura);
-    //     // console.log("GANADERIAP: ", condPGanaderia);
-    //     // console.log("GANADERIAA: ", condAGanaderia);
-    //     // console.log("TAMBOP: ", condPTambo);
-    //     // console.log("TAMBOA: ", condATambo);
-    //     // console.log("MIXTOP: ", condPMixto);
-    //     // console.log("MIXTOA: ", condAMixto);
-
-    //     return [CondP, CondA]
-    //     // return [CondP, CondA, condPAgricultura, condAAgricultura, condPGanaderia, condAGanaderia, condPTambo, condATambo, condPMixto, condAMixto] 
-
-    // };
 
     const addCosecha = () => {
         setAppStage(2);
