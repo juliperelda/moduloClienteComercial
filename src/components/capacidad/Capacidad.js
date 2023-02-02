c2f1d0480e1a34843537e598dd8ec15c96881f18
juliperelda <juliperelda@gmail.com>
2023-02-02 08:23:54 -0300

    cambios

--- src/components/capacidad/Capacidad.js
+++ src/components/capacidad/Capacidad.js
@@ -340,7 +340,8 @@ const Capacidad = () => {
                     style={{ alignItems: "center", boxShadow: "none !important", outline: "0", border: "none !important" }}
                     className="btnEditCosecha"
                     icon={<EditOutlined />}
-                    onClick={() => editarCosecha()}
+                    // onClick={() => editarCosecha()}
+                    onClick={() => console.log(infoCap)}
                     // onChange={(e) => recuperaCosecha(e)}
                     disabled={isButtonEditDisabled}
                 />
