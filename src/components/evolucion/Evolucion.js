d77142d4e111dece9bc5194df67efb406fd472b8
juliperelda <juliperelda@gmail.com>
2023-02-03 08:58:21 -0300

    Cambios en el grafico

--- src/components/evolucion/Evolucion.js
+++ src/components/evolucion/Evolucion.js
@@ -108,7 +108,7 @@ const Evolucion = () => {
             cosecha: item.acos_desc,
             propias: item.ahxs_propias,
             alquiladas: item.ahxs_alquiladas,
-            total: item.ahxs_propias + item.ahxs_alquiladas,
+            total: parseInt(item.ahxs_propias) + parseInt(item.ahxs_alquiladas),
           };
         })
       );
@@ -162,7 +162,7 @@ const Evolucion = () => {
               barSize={50}
               fill="#a9ff96"
               key={"propias"}
-              label={renderCustomBarLabel}
+              // label={renderCustomBarLabel}
               isAnimationActive={true}
             />
           )}
@@ -174,7 +174,7 @@ const Evolucion = () => {
               barSize={50}
               fill="#434348"
               key={"alquiladas"}
-              label={renderCustomBarLabel}
+              // label={renderCustomBarLabel}
               isAnimationActive={true}
             />
           ) : (
@@ -185,7 +185,7 @@ const Evolucion = () => {
               barSize={50}
               fill="#434348"
               key={"alquiladas"}
-              label={renderCustomBarLabel}
+              // label={renderCustomBarLabel}
               isAnimationActive={true}
             />
           )}
