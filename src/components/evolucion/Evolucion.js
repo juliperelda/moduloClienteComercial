d8f349e68c5315e0b7d8cb04777e581a2d43cdb4
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-02-03 09:40:34 -0300

    prueba modificacion de visualizacion variable total en grafico

--- src/components/evolucion/Evolucion.js
+++ src/components/evolucion/Evolucion.js
@@ -100,26 +100,111 @@ const Evolucion = () => {
 
   const [dataForChart, setDataForChart] = useState([]);
 
-  useEffect(() => {
-    if (infoEvo.length > 0) {
-      setDataForChart(
-        infoEvo.map((item) => {
-          return {
-            cosecha: item.acos_desc,
-            propias: item.ahxs_propias,
-            alquiladas: item.ahxs_alquiladas,
-            total: (item.ahxs_propias + item.ahxs_alquiladas),
-          };
-        })
+  // useEffect(() => {
+  //   if (infoEvo.length > 0) {
+  //     setDataForChart(
+  //       infoEvo.map((item) => {
+  //         return {
+  //           cosecha: item.acos_desc,
+  //           propias: item.ahxs_propias,
+  //           alquiladas: item.ahxs_alquiladas,
+  //           total: (item.ahxs_propias + item.ahxs_alquiladas),
+  //         };
+  //       })
+  //     );
+  //   }
+  // }, [infoEvo]);
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active) {
+      return (
+        <div style={{ backgroundColor: "#ccc", padding: "10px" }}>
+          <p>Cosecha: {label}</p>
+          <p>Propias: {payload[0].value}</p>
+          <p>Alquiladas: {payload[1].value}</p>
+          <p>Total: {payload[0].value + payload[1].value}</p>
+        </div>
       );
     }
-  }, [infoEvo]);
+    return null;
+  };
 
 
 
   return (
     <>
-      <ResponsiveContainer className="" width="100%" height={/*400*/ 250}>
+    <ResponsiveContainer className="" width="100%" height={250}>
+      <BarChart
+        width={500}
+        height={300}
+        data={dataForChart}
+        margin={{
+          top: 20,
+          right: 0,
+          left: 0,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="cosecha" />
+        <YAxis
+          label={{ value: "Has.", angle: -90, position: "insideLeft" }}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend
+          iconType="circle"
+          onClick={(x) => handleLegendClick(x)}
+          wrapperStyle={{ fontWeight: "bold", color: "#000000" }}
+        />
+        {isValorPropias ? (
+          <Bar
+            dataKey="propias"
+            name="Propias"
+            stackId="a"
+            barSize={50}
+            fill="#a9ff96"
+            key={"propias"}
+            isAnimationActive={true}
+          />
+        ) : (
+          <Bar
+            dataKey={0}
+            name="Propias"
+            stackId="a"
+            barSize={50}
+            fill="#a9ff96"
+            key={"propias"}
+            isAnimationActive={true}
+          />
+        )}
+        {isValorAlquiladas ? (
+          <Bar
+            dataKey="alquiladas"
+            name="Alquiladas"
+            stackId="a"
+            barSize={50}
+            fill="#434348"
+            key={"alquiladas"}
+            isAnimationActive={true}
+          />
+        ) : (
+          <Bar
+            dataKey={0}
+            name="Alquiladas"
+            stackId="a"
+            barSize={50}
+            fill="#434348"
+            key={"alquiladas"}
+             //label={renderCustomBarLabel}
+             isAnimationActive={true}
+             />
+           )}
+         </BarChart>
+       </ResponsiveContainer>
+
+
+
+      {/* <ResponsiveContainer className="" width="100%" height={250}>
         <BarChart
           width={500}
           height={300}
@@ -190,7 +275,7 @@ const Evolucion = () => {
             />
           )}
         </BarChart>
-      </ResponsiveContainer>
+      </ResponsiveContainer> */}
     </>
   );
 };
