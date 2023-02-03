93af4e22d2e715ddf18868ef54e53d9a437208b5
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-02-03 11:45:31 -0300

    se cambio de color de label y border radius

--- src/components/evolucion/Evolucion.js
+++ src/components/evolucion/Evolucion.js
@@ -123,8 +123,8 @@ const Evolucion = () => {
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
       return (
-        <div className="custom-tooltip" style={{border:"3px solid grey", backgroundColor:"#FFFF", padding:"10px"}}>
-          <p className="label" style={{fontWeight:"500"}}>{`Cosecha: ${label}`}</p>
+        <div className="custom-tooltip" style={{border:"3px solid grey", backgroundColor:"#FFFF", padding:"10px", borderRadius:"4px"}}>
+          <p className="label" style={{color:"grey", fontWeight:"500"}}>{`Cosecha: ${label}`}</p>
           <p className="propias" style={{color:"#a3ef95",fontWeight:"500"}}>{`Propias: ${payload[0].value}`}</p>
           <p className="alquiladas" style={{color:"#434348",fontWeight:"500"}}>{`Alquiladas: ${payload[1].value}`}</p>
           <p className="total" style={{color:"grey",fontWeight:"500"}}>{"Total: " + getIntroOfPage(payload[0].value,payload[1].value)}</p>
