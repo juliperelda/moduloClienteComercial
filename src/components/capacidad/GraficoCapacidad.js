b88ada54b73d04ab366c6c3db59cb66189bb6ffa
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-02-06 10:56:15 -0300

    probando grafico para cuando no hay datos

--- src/components/capacidad/GraficoCapacidad.js
+++ src/components/capacidad/GraficoCapacidad.js
@@ -11,7 +11,7 @@ export const GraficoCapacidad = ({porcentajes}) => {
 
 
     const data = [
-        { name: 'Agricultura', value: parseInt(porcentajes[0].porcentaje) },
+        { name: 'Agricultura', value: parseInt(porcentajes[0].porcentaje)},
         { name: 'Ganaderia', value: parseInt(porcentajes[1].porcentaje) },
         { name: 'Tambo', value: parseInt(porcentajes[2].porcentaje) },
         { name: 'Mixto', value: parseInt(porcentajes[3].porcentaje) },
@@ -21,27 +21,30 @@ export const GraficoCapacidad = ({porcentajes}) => {
 
   return (
     <>
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", padding:"10px"}}>
-      <PieChart width={300} height={250} margin={{ top: 13, left: 50 }}>
-          <Pie
-            data={data ? data : "No hay datos para graficar"}
-            cx={100}
-            cy={100}
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            paddingAngle={5}
-            dataKey="value"
-            label
-          >
-          <Tooltip />
-            {data ? data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            )) : "No hay datos para graficar"}
-          </Pie>
-        </PieChart>
+    {data && data.length > 0 ? (
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center", padding:"10px"}}>
+        <PieChart width={300} height={250} margin={{ top: 13, left: 50 }}>
+            <Pie
+              data={data}
+              cx={100}
+              cy={100}
+              innerRadius={60}
+              outerRadius={80}
+              fill="#8884d8"
+              paddingAngle={5}
+              dataKey="value"
+              label
+            >
+            <Tooltip />
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+          </PieChart>
 
-    </div>
+      </div>
+
+    ): <p>No hay datos</p>}
     </>
   )
 }
